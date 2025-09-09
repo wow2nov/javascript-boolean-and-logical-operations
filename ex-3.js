eb71bda1792889c97ce616fd2a786bcca01f85c6
Supapond <supapondmaliwab@gmail.com>
2025-09-09 12:59:46 +0700

    fead(ex-3):edit debug

--- ex-3.js
+++ ex-3.js
@@ -1,9 +1,10 @@
 //Exercise #3: Debugging (Boolean & Logical Operations)
 
 //Start coding here
-const numberA = 100;
-const numberB = "20";
+let numberA = 100;
+let numberB = "20";
 
 numberA = 10;
+numberB = Number(numberB)
 
 console.log(`Sum: ${numberA + numberB}`);
